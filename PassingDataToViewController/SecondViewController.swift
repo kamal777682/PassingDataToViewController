f931fd1dcc63b53ec1d12d4c5670b16cc5f07a69
kamalpreet <kamal7776825>
2020-03-03 19:20:54 -0500

    [all close button]

---
+++ PassingDataToViewController/SecondViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SecondViewController.swift
+//  PassingDataToViewController
+//
+//  Created by MacStudent on 2020-03-03.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var lblWelcome: UILabel!
+    var name : String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Hide Back Button from navigation Bar
+        self.navigationItem.hidesBackButton = true
+        if let nm = name{
+            self.lblWelcome.text = "Welcome, \(nm)"
+            self.navigationItem.title = "Welcome ,\(nm)"
+        }
+        else
+        {
+            self.lblWelcome.text = "No Name Sent from first VC"
+        }
+        }
+
+    private func addCloseButton()
+    {
+        let btnClose = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(self.goBack))
+        self.navigationItem.ri
+    }
+    @objc func goBack()
+    {
+        self.navigationController?.popViewController(animated: true)
+    }
+    // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- PassingDataToViewController/ViewController.swift
+++ PassingDataToViewController/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func btnGoNextx(_ sender: UIButton)
+    {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = sb.instantiateViewController(identifier: "secondVC") as! SecondViewController
+        if let name = self.textName.text
+        {
+            secondVC.name = name
+            navigationController?.pushViewController(secondVC, animated: true)
+            //self.present(secondVC, animated: true , completion : nil)
+        }
+    }
 }
 
